d57d0e8dfc6df073b74b7387bb08bc679f1e7fc8
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-15 03:25:31 +1300

    Added the price div info to body component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/CardBody.jsx
+++ src/Components/CardBody.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyledCardBody } from '../Styled'
-import { Button } from './Button'
+import { Button } from './index'
 
 const CardBody = () => {
 	return (
@@ -12,7 +12,12 @@ const CardBody = () => {
 					A floral, solar and voluptuous interpretation composed by Olivier
 					Polge, Perfumer-Creator for the House of CHANEL.
 				</p>
-
+				<div className='price'>
+					<span className='main-price'>$149.99</span>
+					<span>
+						<s>$169.99</s>
+					</span>
+				</div>
 				<Button />
 			</StyledCardBody>
 		</>
