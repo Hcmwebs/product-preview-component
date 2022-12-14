fb96ec6d82c1e3113076c6145024100a1815c6db
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-14 23:52:13 +1300

    Updated the card data
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/CardBody.jsx
+++ src/Components/CardBody.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import {StyledCardBody} from '../Styled'
+import { StyledCardBody } from '../Styled'
+import { Button } from './Button'
 
 const CardBody = () => {
 	return (
 		<>
 			<StyledCardBody>
-				<h1>Improve your front-end skills by building projects</h1>
+				<h1>Perfume</h1>
+				<h2>Gabrielle Essence Eau De Parfum</h2>
 				<p>
-					Scan the QR code to visit Frontend Mentor and take your coding skills
-					to the next level
+					A floral, solar and voluptuous interpretation composed by Olivier
+					Polge, Perfumer-Creator for the House of CHANEL.
 				</p>
+
+				<Button />
 			</StyledCardBody>
 		</>
 	)
