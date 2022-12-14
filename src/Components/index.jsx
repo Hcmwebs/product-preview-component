1e364e3a4e4b77d42da08f326efaefc8a70b28a3
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-14 23:54:19 +1300

    Created and Indexed the button
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,5 +1,6 @@
 import Card from './Card'
 import CardHeader from './CardHeader'
 import CardBody from './CardBody'
+import Button from './Button'
 
-export { Card, CardHeader, CardBody }
+export { Card, CardHeader, CardBody, Button }
