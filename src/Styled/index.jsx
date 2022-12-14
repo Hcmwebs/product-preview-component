3665c88f104636972fbc28be60e74f470cff15d0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-14 23:53:49 +1300

    created and indexed a styled button
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Styled/index.jsx
+++ src/Styled/index.jsx
@@ -3,6 +3,13 @@ import GlobalStyle from './GlobalStyle'
 import StyledCard from './StyledCard'
 import StyledCardHeader from './StyledCardHeader'
 import StyledCardBody from './StyledCardBody'
+import StyledButton from './StyledButton'
 
-
-export {Container, GlobalStyle,StyledCard,StyledCardHeader,StyledCardBody }
+export {
+	Container,
+	GlobalStyle,
+	StyledCard,
+	StyledCardHeader,
+	StyledCardBody,
+	StyledButton,
+}
